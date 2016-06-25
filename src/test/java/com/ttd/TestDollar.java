46a3a94b22b12e4102a63e5af544c7945bb1c987
Grzegorz Sledz <grzegorz_sledz@epam.com>
2016-06-25 23:04:35 +0200

    Chapter 2

--- src/test/java/com/ttd/TestDollar.java
+++ src/test/java/com/ttd/TestDollar.java
@@ -11,7 +11,9 @@ public class TestDollar {
     @Test
     public void testMulitpilication(){
         Dollar five=new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(10,five.amount);
+        Dollar product=five.times(2);
+        Assert.assertEquals(10,product.amount);
+        product=five.times(3);
+        Assert.assertEquals(15,product.amount);
     }
 }
